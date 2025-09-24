2fb45042eecca43115ac79a2ee0a4686ba54124f
will-lin2021 <2021linw39@gmail.com>
2025-09-24 10:54:34 -0500

    connected all take routes to database; added function documentation and request body schema and response; added utils module

--- app.py
+++ app.py
@@ -2,16 +2,23 @@ import os
 
 from flask import Flask, render_template, request, send_from_directory;
 
+from psycopg2.errors import ForeignKeyViolation
+
 from db import setup as db_setup
 import db.temp as db_query
+from utils import check_db_for_take, check_db_for_user, check_db_for_user_match, convert_dict_keys_to_camelCase
 
+# Flask app setup
 app = Flask(__name__)
 app.static_folder = "static"
 
-# with app.app_context():
-#     db_setup()
+# Setup database
+with app.app_context():
+    db_setup()
 
-USER_ID = "00000000-0000-0000-0000-000000000000"
+USER_ID = "00000000-0000-0000-0000-000000000001"
+
+TAKE_COLS = ["title", "tagId", "description"]
 
 takes = [
     {
@@ -32,10 +39,11 @@ takes = [
     }
 ]
 
-@app.route("/healthcheck")
+@app.route("/health")
 def healthcheck():
     return {
-
+        "version": "0.0.1",
+        "message": "Hot Takes Web Server",
     }
 
 @app.route("/favicon.ico")
@@ -60,6 +68,18 @@ API Routes
 - User registration
 - Take query and CRUD routes
 - Take comment query and CUD routes
+
+Take Response Format:
+{
+    takeId: string,
+    title: string,
+    newTitle: string | undefined,
+    tag: string,
+    description: string,
+    author: string,
+    createdOn: date,
+    updatedOn: date,
+}
 """
 
 # User Routes
@@ -84,102 +104,223 @@ def user_add_session():
 def take_query():
     """
     Handle request to query takes
-    """
-    # NOTE: pages are 1-indexed [1, 2, ...]
 
+    Query Parameters:
+        query (string): search query
+        page (int): page number (1-indexed)
+        limit (int): number of takes to query
+
+    Returns:
+        dict: number of takes queried and list of takes with HTTP status code 200
+    """
+
+    # Get request parameters
     query = request.args.get("query", "")
     page = request.args.get("page", 1, type=int)
     limit = request.args.get("limit", 25, type=int)
 
+    # Set query offset
     offset = (page - 1) * limit
 
+    # Run database query
     takes = db_query.take_query(limit=limit, offset=offset, search_query=query)
 
     return {
         "count": len(takes),
-        "data": takes,
+        "data": list(map(convert_dict_keys_to_camelCase, takes)),
     }
 
-@app.route("/api/takes/create", methods=["POST"])
+@app.route("/api/takes", methods=["POST"])
 def take_create():
     """
     Handle request to create a new take
 
-    Body Format::
+    This API endpoint is protected and requires authorization
 
-        {
+    Body:
+        json: {
             title: string,
-            tags: string,
+            tagId: number,
             description: string | null,
         }
+
+    Returns:
+        tuple: created take with status 201, or error message with error HTTP status code
+
+        Status Codes:
+        201: creation success
+        400: request errors (invalid user ID, invalid tag ID, etc.)
+        401: user is not authenticated (logged-out)
     """
 
+    # Get Authorization header and user ID
+    userId = request.headers.get("Authorization")
+    if userId is None:
+        return "Not authenticated", 401
+    userId = userId.removeprefix("Bearer ")
+
+    # Check if user exists; return unauthorized to hide whether user exists
+    if not check_db_for_user(userId):
+        return "User not authenticated", 401
+
+    # Get request body and verify required values
     body = dict(request.get_json())
-    print(body)
+    bodyKeys = body.keys()
+    if "title" not in bodyKeys or "tagId" not in bodyKeys:
+        return "Missing values in request body", 400
 
-    take = db_query.take_insert(USER_ID, title=body.get("title"), tag=body.get("tags"), description=body.get("description"))
+    # Run database query
+    try:
+        take = db_query.take_insert(userId, title=body.get("title"), tag_id=body.get("tagId"), description=body.get("description"))
+    except ForeignKeyViolation:
+        return "Invalid tag ID", 400
 
-    return take
+    return convert_dict_keys_to_camelCase(take), 201
 
-@app.route("/api/takes/<uuid:takeId>")
+@app.route("/api/takes/<takeId>")
 def take_retrieve(takeId):
     """
     Handle request to retrieve a take
+
+    Args:
+        takeId: take UUID
+
+    Returns:
+        take: retrieved take with response code 200
+
+        If take is not found, return status code 404
     """
 
-    # TODO
+    # Check if take exists
+    if not check_db_for_take(takeId):
+        return "Take not found", 404
 
-    take = db_query.take_select(USER_ID, takeId)
+    # Run database query
+    take = db_query.take_select(takeId)
 
-    return take
+    return convert_dict_keys_to_camelCase(take), 200
 
-@app.route("/api/takes/<uuid:takeId>", methods=["PATCH"])
+@app.route("/api/takes/<takeId>", methods=["PATCH"])
 def take_update(takeId):
     """
-    Handle request to update take
+    Handle request to update a take
+
+    This API endpoint is protected and requires authorization
+
+    Args:
+        takeId: take UUID
+
+    Body:
+        json: {
+            title: string | null,
+            tagId: string | null,
+            description | null,
+        }
+
+    Returns:
+        tuple: response message and HTTP status code
+
+        Status Codes:
+        200: update success
+        400: request errors (user not found, no changes requested, etc)
+        401: User is not authenticated (logged-out)
+        404: take not found
     """
 
-    # TODO
+    # Get Authorization header and user ID
+    userId = request.headers.get("Authorization")
+    if userId is None:
+        return "Not authenticated", 401
+    userId = userId.removeprefix("Bearer ")
 
+    # Check if user exists; return unauthorized to hide whether user exists
+    if not check_db_for_user(userId):
+        return "User not authenticated", 401
+
+    # Check if take exists
+    if not check_db_for_take(takeId):
+        return "Take not found", 404
+
+    # Check if user has perms to update take
+    if not check_db_for_user_match(userId, takeId):
+        return "Not authorized", 403
+
+    # Get request body and verify updates are requested
     body = dict(request.get_json())
-    print(body)
+    if not any(key in body.keys() for key in TAKE_COLS):
+        return "No values in request body", 400
 
-    take = db_query.take_update(USER_ID, takeId, title=body.get("title"), tag=body.get("tag"), description=body.get("description"))
+    # Run database query
+    try:
+        take = db_query.take_update(USER_ID, takeId, title=body.get("title"), tag_id=body.get("tagId"), description=body.get("description"))
+    except ForeignKeyViolation:
+        return "Invalid tag ID", 400
 
-    return take
+    return convert_dict_keys_to_camelCase(take), 200
 
-@app.route("/api/takes/<uuid:takeId>", methods=["DELETE"])
+@app.route("/api/takes/<takeId>", methods=["DELETE"])
 def take_delete(takeId):
     """
     Handle request to delete take
+
+    This API endpoint is protected and requires authorization
+
+    Args:
+        takeId (str): take UUID
+
+    Returns:
+        tuple: response message and HTTP status code
+
+        Status Codes:
+        204: if take was deleted
+        404: if take was not found
+        500: if take was not deleted due to server errors
     """
 
-    # TODO
+    # Get Authorization header and user ID
+    userId = request.headers.get("Authorization")
+    if userId is None:
+        return "Not authenticated", 401
+    userId = userId.removeprefix("Bearer ")
 
-    succ = db_query.take_delete(USER_ID, takeId)
-    if succ:
-        return "", 204
-    else:
-        return "", 400
+    # Check if user exists; return unauthorized to hide wheterh user exists
+    if not check_db_for_user(userId):
+        return "User not authenticated", 401
+
+    # Check if take exists
+    if not check_db_for_take(takeId):
+        return "Take not found", 404
+
+    # Check if user has perms to delete take
+    if not check_db_for_user_match(userId, takeId):
+        return "Not authorized", 403
+
+    # Run database query
+    res = db_query.take_delete(userId, takeId)
+
+    if not res:
+        return "Take not deleted", 500
+
+    return "", 204
 
 # Take Comment Routes
-@app.route("/api/takes/<uuid:takeId>/comments")
+@app.route("/api/takes/<takeId>/comments")
 def take_comment_query(takeId):
     # TODO
     # NOTE: default to newest to oldest, but allow filtering?
     pass
 
-@app.route("/api/takes/<uuid:takeId>/comments/add")
+@app.route("/api/takes/<takeId>/comments/add")
 def take_comment_create(takeId):
     # TODO
     pass
 
-@app.route("/api/takes/<uuid:takeId>/comments/<uuid:commentId>")
+@app.route("/api/takes/<takeId>/comments/<commentId>")
 def take_comment_update(takeId, commentId):
     # TODO
     pass
 
-@app.route("/api/takes/<uuid:takeId>/comments/<uuid:commentId>")
+@app.route("/api/takes/<takeId>/comments/<commentId>")
 def take_comment_delete(takeId, commentId):
     # TODO
     pass
--- db/temp/__init__.py
+++ db/temp/__init__.py
@@ -2,11 +2,24 @@ from datetime import datetime, timezone
 
 from db import get_db_cursor
 
-# TODO: make function to check if user exists
-# TODO: make function to check if take exists
-# TODO: catch Integrity Errors
-#   - user id not found
-#   - tag id not found in tag table (insert or update)
+from utils import psycopg2_res_to_dict
+
+TAKE_RETURN = "take_id, title, newtitle, label AS tag, description, username as author, created_on, updated_on"
+"""
+Columns to return in SQL
+
+Take Return Format:
+{
+    take_id: string,
+    title: string,
+    new_title: string | undefined,
+    tag: string,
+    description: string,
+    author: string,
+    created_on: date,
+    updated_on: date,
+}
+"""
 
 def take_query(limit, offset, search_query=None):
     """
@@ -18,138 +31,177 @@ def take_query(limit, offset, search_query=None):
         search_query (str): title search query
 
     Returns:
-        takes: Return list of takes in query
+        list: A list containing dictionaries of the take
 
         Takes returned from query are the `offset+1`th take to the `offset+limit+1`th takes
     """
 
     # Query takes
-    data = ()
     if search_query:
-        sqlQuery = f"SELECT * FROM takes WHERE title LIKE '%' || %s || '%' LIMIT {limit} OFFSET {offset};"
-        data = (search_query,)
+        sqlQuery = (f"SELECT {TAKE_RETURN} "
+                    "FROM hot_takes.takes "
+                    "JOIN hot_takes.tags USING (tag_id) "
+                    "JOIN hot_takes.user_accounts ON author_id=user_id "
+                    "WHERE title ILIKE %s"
+                    "ORDER BY created_on DESC "
+                    f"LIMIT {limit} OFFSET {offset};")
+        data = (f"%{search_query}%",)
     else:
-        sqlQuery = f"SELECT * FROM takes LIMIT {limit} OFFSET {offset};"
+        sqlQuery = (f"SELECT {TAKE_RETURN} "
+                    "FROM hot_takes.takes "
+                    "JOIN hot_takes.tags USING (tag_id) "
+                    "JOIN hot_takes.user_accounts ON author_id=user_id "
+                    "ORDER BY created_on DESC "
+                    f"LIMIT {limit} OFFSET {offset};")
+        data = ()
 
-    print(sqlQuery)
+    with get_db_cursor() as cursor:
+        cursor.execute(sqlQuery, data)
+        rows = cursor.fetchall()
 
-    # TODO: catch Integrity
-    takes = []
+    return list(map(psycopg2_res_to_dict, rows))
 
-    return takes
-
-def take_insert(userId, title, tag, description=None):
+def take_insert(user_id, title, tag_id, description=None):
     """
     Inserts a new take into the database
 
     Args:
-        userId (str): User UUID
+        user_id (str): User UUID
         title (str): Title of new take
-        tag (str): Tag given to new take
+        tag_id (str): Tag ID
         description (str, optional): Description for new take
 
     Returns:
-        take: Return new take
+        dict: created take
     """
 
     # Insert take
-    sqlQuery = f"INSERT * INTO takes VALUES (user_id=%s, title=%s, description=%s, tag=%s);"
-    data = (userId, title, tag, description,)
-    print(sqlQuery)
+    sqlQuery = ("INSERT INTO hot_takes.takes (author_id, title, tag_id, description) "
+                "VALUES (%s, %s, %s, %s) "
+                "RETURNING take_id;")
+    data = (user_id, title, tag_id, description,)
 
-    return {}
+    with get_db_cursor(commit=True) as cursor:
+        cursor.execute(sqlQuery, data)
+        row = cursor.fetchone()
 
-def take_select(userId, takeId):
+    # Get created take
+    take = take_select(row[0])
+
+    return take
+
+def take_select(take_id):
     """
     Retrieves a take with a given ID from database
 
     Args:
-        userId (str): User UUID
-        takeId (str): Take UUID
+        take_id (str): Take UUID
 
     Returns:
-        take: If exists, return take
-        None: Else, return None
+        dict: retrieved take
     """
 
     # Select take
-    sqlQuery = f"SELECT * FROM takes WHERE user_id=%s AND take_id=%s;"
-    data = (userId, takeId,)
-    print(sqlQuery)
+    sqlQuery = (f"SELECT {TAKE_RETURN} "
+                "FROM hot_takes.takes "
+                "JOIN hot_takes.tags USING (tag_id) "
+                "JOIN hot_takes.user_accounts ON author_id=user_id "
+                "WHERE take_id=%s;")
+    data = (take_id,)
 
-    take = {}
+    with get_db_cursor() as cursor:
+        cursor.execute(sqlQuery, data)
+        row = cursor.fetchone()
 
-    return take
+    return psycopg2_res_to_dict(row)
 
-def take_update(userId, takeId, title=None, tag=None, description=None):
+def take_update(user_id, take_id, title=None, tag_id=None, description=None):
     """
     Updates a take with a given ID in database
 
     Args:
-        userId (str): User UUID
-        takeId (str): Take UUID
+        user_id (str): User UUID
+        take_id (str): Take UUID
         title (str, optional): New title
-        tag (str, optional): New tag
+        tag_id (str, optional): Tag ID
         description (str, optional): New description
 
     Returns:
-        take: If exists and changes are made, return updated take
-        None: else, return None
+        dict: updated take
+
     """
 
-    updatedOn = datetime.now(timezone.utc).astimezone()
-    print(updatedOn)
-
-    if not title and not tag and not description:
-        return None
-
     # Update take
-    sqlQuery = f"UPDATE takes SET updated_on=%s,"
-    data = (updatedOn,)
+    sqlQuery = "UPDATE hot_takes.takes SET updated_on=%s,"
+    data = (datetime.now(timezone.utc).astimezone(),)
+    updated = False
     if title:
         sqlQuery += " title=%s"
         data += (title,)
-    if tag:
-        sqlQuery += " tag=%s"
-        data += (tag,)
+        updated = True
+    if tag_id:
+        if updated: sqlQuery += ","
+
+        sqlQuery += " tag_id=%s"
+        data += (tag_id,)
+        updated = True
+
     if description:
+        if updated: sqlQuery += ","
+
         sqlQuery += " description=%s"
         data += (description,)
-    sqlQuery += " WHERE user_id=%s AND take_id=%s;"
-    data += (userId, takeId,)
-    print(sqlQuery)
+    sqlQuery += " WHERE author_id=%s AND take_id=%s;"
+    data += (user_id, take_id,)
 
-    take = {}
+    with get_db_cursor(commit=True) as cursor:
+        cursor.execute(sqlQuery, data)
+
+    # Get updated take
+    take = take_select(take_id)
 
     return take
 
-def take_delete(userId, takeId):
+def take_delete(user_id, take_id):
     """
     Removes a take from the database
 
     Args:
-        userId (str): User UUID
-        takeId (str): Take UUID
+        user_id (str): User UUID
+        take_id (str): Take UUID
 
     Returns:
-        boolean: `True` on success; `False` on failure
-        None: If take does not exist
+        bool: take deletion success
+
+        `True` on success, `False` on failure
+
+    Raise:
+        RuntimeError: If database deletes more than 1 value; this also rollsback any changes
     """
 
-    sqlQuery = f"DELETE FROM takes WHERE user_id=%s AND take_id=%s"
-    data = (userId, takeId,)
-    print(sqlQuery)
+    sqlQuery = ("DELETE FROM hot_takes.takes "
+                "WHERE author_id=%s AND take_id=%s;")
+    data = (user_id, take_id,)
+
+    with get_db_cursor(commit=True) as cursor:
+        cursor.execute(sqlQuery, data)
+
+        if cursor.rowcount > 1:
+            raise RuntimeError("Database deleted more 1 value")
+
+        if cursor.rowcount == 0:
+            return False
 
     return True
 
-def take_comment_query(takeId):
+def take_comment_query(take_id):
     return []
 
-def take_comment_insert(userId, takeId, comment=None):
+def take_comment_insert(user_id, take_id, comment=None):
     pass
 
-def take_comment_update(userId, takeId, commentId, comment=None):
+def take_comment_update(user_id, take_id, comment_id, comment=None):
     pass
 
-def take_comment_delete(userId, takeId, commentId):
+def take_comment_delete(user_id, take_id, comment_id):
     pass
