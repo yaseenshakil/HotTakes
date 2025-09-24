9f591f737536ee09dc52cd715a89000d588f7f54
will-lin2021 <2021linw39@gmail.com>
2025-09-24 10:49:01 -0500

    changed all columns to snake case, separated all tables into separate queries (instead of in schema create query), added tags and all preset tags to schema

--- schema.up.sql
+++ schema.up.sql
@@ -1,4 +1,5 @@
-CREATE SCHEMA hot_takes AUTHORIZATION csci5117
+-- Add schema for user
+CREATE SCHEMA hot_takes AUTHORIZATION csci5117;
 
 -- Create users super-class
 CREATE TABLE IF NOT EXISTS hot_takes.users
@@ -13,7 +14,7 @@ CREATE TABLE IF NOT EXISTS hot_takes.users
     updated_on timestamptz(0) DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (user_id)
-)
+);
 -- Create account user table
 CREATE TABLE IF NOT EXISTS hot_takes.user_accounts
 (
@@ -21,8 +22,8 @@ CREATE TABLE IF NOT EXISTS hot_takes.user_accounts
 
     username text NOT NULL,
 
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-)
+    FOREIGN KEY (user_id) REFERENCES hot_takes.users(user_id)
+);
 -- Create session user table
 CREATE TABLE IF NOT EXISTS hot_takes.session_users
 (
@@ -30,25 +31,34 @@ CREATE TABLE IF NOT EXISTS hot_takes.session_users
 
     token text NOT NULL,
 
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-)
+    FOREIGN KEY (user_id) REFERENCES hot_takes.users(user_id)
+);
+-- Add category type for tags
+CREATE TYPE hot_takes.category AS ENUM (
+  '🎬 Entertainment',
+  '🏆 Sports',
+  '🍔 Food & Lifestyle',
+  '💻 Tech & Business',
+  '🌍 Society & Current Events',
+  '📚 Academics'
+);
 -- Create tags table
 CREATE TABLE IF NOT EXISTS hot_takes.tags
 (
-    tag_id serial,
+    tag_id serial NOT NULL,
 
-    label text,
-    category text,
+    label text NOT NULL,
+    category hot_takes.category NOT NULL,
 
     PRIMARY KEY (tag_id)
-)
+);
 -- Create takes table
 CREATE TABLE IF NOT EXISTS hot_takes.takes
 (
     take_id uuid NOT NULL DEFAULT gen_random_uuid(),
 
     title text NOT NULL,
-    newtitle text,
+    new_title text,
 
     description text,
     tag_id integer NOT NULL,
@@ -59,30 +69,31 @@ CREATE TABLE IF NOT EXISTS hot_takes.takes
     updated_on timestamptz(0) DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (take_id),
-    FOREIGN KEY (tag_id) REFERENCES tags(tag_id),
-    FOREIGN KEY (author_id) REFERENCES users(user_id)
-)
+    FOREIGN KEY (tag_id) REFERENCES hot_takes.tags(tag_id),
+    FOREIGN KEY (author_id) REFERENCES hot_takes.users(user_id)
+);
 -- Create take-rating table
 CREATE TABLE IF NOT EXISTS hot_takes.take_ratings
 (
     take_id uuid NOT NULL,
     user_id uuid NOT NULL,
 
-    rating numeric(3, 1), -- limits to 1 decimal place
+    -- limit to 1 decimal place
+    rating numeric(3, 1) NOT NULL,
 
     created_on timestamptz(0) DEFAULT CURRENT_TIMESTAMP,
     updated_on timestamptz(0) DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (user_id, take_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (take_id) REFERENCES takes(take_id)
-)
+    FOREIGN KEY (user_id) REFERENCES hot_takes.users(user_id),
+    FOREIGN KEY (take_id) REFERENCES hot_takes.takes(take_id)
+);
 -- Create take-comments table
 CREATE TABLE IF NOT EXISTS hot_takes.take_comments
 (
-    take_id uuid,
-    commenter_id uuid,
-    comment_id serial,
+    take_id uuid NOT NULL,
+    commenter_id uuid NOT NULL,
+    comment_id serial NOT NULL,
 
     comment text,
 
@@ -90,6 +101,52 @@ CREATE TABLE IF NOT EXISTS hot_takes.take_comments
     updated_on timestamptz(0) DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (take_id, commenter_id, comment_id),
-    FOREIGN KEY (take_id) REFERENCES takes(take_id),
-    FOREIGN KEY (commenter_id) REFERENCES users(user_id)
+    FOREIGN KEY (take_id) REFERENCES hot_takes.takes(take_id),
+    FOREIGN KEY (commenter_id) REFERENCES hot_takes.users(user_id)
 );
+
+INSERT INTO tags (category, value)
+VALUES
+  ('🎬 Entertainment', 'movies'),
+  ('🎬 Entertainment', 'tv-shows'),
+  ('🎬 Entertainment', 'pop-culture'),
+  ('🎬 Entertainment', 'celebrities'),
+  ('🎬 Entertainment', 'books'),
+  ('🎬 Entertainment', 'anime'),
+  ('🎬 Entertainment', 'cartoons'),
+  ('🎬 Entertainment', 'award-shows'),
+  ('🎬 Entertainment', 'memes'),
+  ('🎬 Entertainment', 'video-games'),
+
+  ('🏆 Sports', 'sports'),
+  ('🏆 Sports', 'nfl'),
+  ('🏆 Sports', 'nba'),
+  ('🏆 Sports', 'nhl'),
+  ('🏆 Sports', 'olympics'),
+
+  ('🍔 Food & Lifestyle', 'food'),
+  ('🍔 Food & Lifestyle', 'fast-food'),
+  ('🍔 Food & Lifestyle', 'restaurants'),
+  ('🍔 Food & Lifestyle', 'cooking'),
+  ('🍔 Food & Lifestyle', 'relationships'),
+  ('🍔 Food & Lifestyle', 'dating'),
+  ('🍔 Food & Lifestyle', 'fashion'),
+  ('🍔 Food & Lifestyle', 'travel'),
+  ('🍔 Food & Lifestyle', 'health'),
+  ('🍔 Food & Lifestyle', 'animals'),
+  ('🍔 Food & Lifestyle', 'pets'),
+
+  ('💻 Tech & Business', 'social-media'),
+  ('💻 Tech & Business', 'ai'),
+  ('💻 Tech & Business', 'hardware'),
+  ('💻 Tech & Business', 'finance'),
+  ('💻 Tech & Business', 'technology'),
+
+  ('🌍 Society & Current Events', 'climate-change'),
+  ('🌍 Society & Current Events', 'current-events'),
+  ('🌍 Society & Current Events', 'news'),
+  ('🌍 Society & Current Events', 'geography'),
+  ('🌍 Society & Current Events', 'history'),
+
+  ('📚 Academics', 'science'),
+  ('📚 Academics', 'math');
